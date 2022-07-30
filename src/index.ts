ea85ca243a43e0f9a70b87493e30e749e03b24cf
Andre <andreddavies@hotmail.com>
2022-07-30 13:32:24 -0300

    Removing route from index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import { app } from "@app";
 import config from "@config";
 import { errorMiddleware } from "@middlewares/error";
 
-app.get("/", (_, res) => {
-  res.send("Hello, world");
-});
-
 app.use(errorMiddleware);
 
 app.listen(config.app.port, () => {
