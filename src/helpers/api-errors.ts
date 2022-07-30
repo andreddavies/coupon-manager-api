7e18603c7110588fda85acea52657676a759e495
Andre <andreddavies@hotmail.com>
2022-07-30 17:53:46 -0300

    Setting optional params

--- src/helpers/api-errors.ts
+++ src/helpers/api-errors.ts
@@ -1,26 +1,26 @@
 export class ApiError extends Error {
   public readonly statusCode: number;
 
-  constructor(statusCode: number, message: string) {
+  constructor(message?: string, statusCode?: number) {
     super(message);
     this.statusCode = statusCode;
   }
 }
 
 export class BadRequestError extends ApiError {
-  constructor(message: string) {
-    super(400, message);
+  constructor(message?: string) {
+    super(message, 400);
   }
 }
 
 export class UnauthorizedError extends ApiError {
-  constructor(message: string) {
-    super(401, message);
+  constructor(message?: string) {
+    super(message, 401);
   }
 }
 
 export class NotFoundError extends ApiError {
-  constructor(message: string) {
-    super(404, message);
+  constructor(message?: string) {
+    super(message, 404);
   }
 }
