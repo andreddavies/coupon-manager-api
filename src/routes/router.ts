fee669b748504d9c0c85e132dcf4c70764e33696
Andre <andreddavies@hotmail.com>
2022-07-30 14:32:10 -0300

    Adding a route to find an user in db

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,8 +2,12 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+import UserController from "@controllers/UserController";
+
+router.get("/api", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
+
+router.get("/api/user/:id", UserController.findOne);
 
 export default router;
