24449e8be6e1861e009ddf1c3bfd56f61d6f3bad
Andre <andreddavies@hotmail.com>
2022-07-30 02:04:33 -0300

    Setting app to use errors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { app } from "@app";
 import config from "@config";
+import { errorMiddleware } from "@middlewares/error";
 
 app.get("/", (_, res) => {
   res.send("Hello, world");
 });
 
+app.use(errorMiddleware);
+
 app.listen(config.app.port, () => {
-  console.log("Server running...");
+  console.log("Servidor rodando...");
 });
