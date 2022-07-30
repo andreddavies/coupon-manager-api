dd8cca2839a94ce6ea001cb04264fbf05c9305a0
Andre <andreddavies@hotmail.com>
2022-07-30 19:42:49 -0300

    Creating coupon routes and adding coupon controller methods

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 import UserController from "@controllers/UserController";
+import CouponController from "@controllers/CouponController";
 
 router.get("/api", (req: Request, res: Response) => {
   res.send("Hello world!");
@@ -13,5 +14,11 @@ router.post("/api/user/signup", UserController.create);
 router.post("/api/user/signin", UserController.login);
 router.patch("/api/user/:id", UserController.update);
 router.delete("/api/user/:id", UserController.delete);
+
+router.get("/api/coupon", CouponController.list);
+router.get("/api/coupon/:id", CouponController.getCoupon);
+router.post("/api/coupon", CouponController.create);
+router.patch("/api/coupon/:id", CouponController.update);
+router.delete("/api/coupon/:id", CouponController.delete);
 
 export default router;
