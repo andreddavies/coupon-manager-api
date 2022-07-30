4c21e641b3117e80ea8c0ca30130ee62655ab9ee
Andre <andreddavies@hotmail.com>
2022-07-30 17:54:37 -0300

    Creating methods to create user and login. Updating method to update and delete user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,18 +2,75 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+import config from "@config";
 import User from "@schemas/User";
 import { ApiError, BadRequestError } from "@helpers/api-errors";
 
 interface IUserData {
-  name?: string;
-  email?: string;
-  password?: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 type UserControllerResponse = Response | ApiError;
 
 class UserController {
+  async create(req: Request, res: Response): Promise<UserControllerResponse> {
+    const { name, email, password }: IUserData = req.body;
+
+    if (!name || !email || !password)
+      throw new BadRequestError("Todos os campos são obrigatórios!");
+
+    const user = await User.findOne({ email });
+    if (user) throw new BadRequestError("Este e-mail já está sendo utilizado!");
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    return await User.create({
+      name,
+      email,
+      password: passwordHash,
+    })
+      .then(async (response) => {
+        const finalUser = await User.findById(response._id, "-password");
+
+        return res.json({ user: finalUser });
+      })
+      .catch((err) => {
+        throw new ApiError(err);
+      });
+  }
+
+  async login(req: Request, res: Response): Promise<UserControllerResponse> {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      throw new BadRequestError("Todos os campos são obrigatórios!");
+
+    const user = await User.findOne({ email });
+    if (!user) throw new BadRequestError("Usuário não existe!");
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) throw new BadRequestError("Senha inválida");
+    else {
+      const { secret } = config;
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        secret
+      );
+
+      return res.json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+      });
+    }
+  }
+
   async findOne(req: Request, res: Response): Promise<UserControllerResponse> {
     const { id } = req.params;
     const user = await User.findById(id, "-password");
@@ -24,21 +81,32 @@ class UserController {
 
   async update(req: Request, res: Response): Promise<UserControllerResponse> {
     const { id } = req.params;
-    const data: IUserData = req.body;
-    const user = await User.findByIdAndUpdate(
-      id,
-      { ...data },
-      { overwrite: true }
-    );
+    const { email, name }: IUserData = req.body;
+    const user = await User.findByIdAndUpdate(id, { email, name });
 
-    console.log(user);
-    if (user) return res.json({ user });
-    else throw new BadRequestError("Não foi possível atualizar o usuário!");
+    if (user) {
+      const { password }: IUserData = req.body;
+
+      if (password) {
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        await User.findByIdAndUpdate(id, { password: passwordHash })
+          .then(async (response) => {
+            const returnData = await User.findById(response._id, "-password");
+
+            return res.json({ user: returnData });
+          })
+          .catch((err) => {
+            throw new ApiError(err);
+          });
+      } else return res.json({ user });
+    } else throw new BadRequestError("Não foi possível atualizar o usuário!");
   }
 
   async delete(req: Request, res: Response): Promise<UserControllerResponse> {
     const { id } = req.params;
-    const deleteUser = await User.deleteOne({ id });
+    const deleteUser = await User.findByIdAndDelete(id);
 
     if (deleteUser)
       return res.json({ message: "Usuário foi removido com sucesso!" });
