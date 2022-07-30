76c433ee3d17ab8a857c22f8c9bb01b9971f4fcc
Andre <andreddavies@hotmail.com>
2022-07-30 01:56:08 -0300

    Setting config variables based on .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,30 @@
+interface IApp {
+  port: string | number;
+}
+
+interface IDB {
+  dbUser: string;
+  dbPass: string;
+  dbHost: string;
+  dbPort: string;
+  dbName: string;
+}
+
 interface IConfig {
-  app: { port: string | number };
+  app: IApp;
+  db: IDB;
 }
 
 const config: IConfig = {
   app: {
     port: process.env.PORT || 4000,
+  },
+  db: {
+    dbUser: process.env.DB_USER,
+    dbPass: process.env.DB_PASS,
+    dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT,
+    dbName: process.env.DB_NAME,
   },
 };
 
