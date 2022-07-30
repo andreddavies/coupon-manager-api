d1083142a874e50957e58d2bf177b8f0976ee413
Andre <andreddavies@hotmail.com>
2022-07-30 02:09:14 -0300

    Setting app to use router

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import cors from "cors";
 import "dotenv/config";
 
 import "./database";
+import router from "@routes/router";
 
 const app: express.Application = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+app.use(router);
 
 export { app };
