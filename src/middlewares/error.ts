6c7abdaefd908f3c9f296b710c88dbb7a591006e
Andre <andreddavies@hotmail.com>
2022-08-01 03:11:28 -0300

    Creating basic rules to handle duplicate keys and required fields for now

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -12,5 +12,19 @@ export const errorMiddleware = (
     : "Erro interno do servidor!";
   const statusCode = error.statusCode ?? 500;
 
+  if (message.indexOf("duplicate key") !== -1) {
+    const error = message.split("dup key: { ")[1].split(":")[0];
+
+    return res
+      .status(statusCode)
+      .json({ message: `Campo ${error} com chave duplicada!` });
+  }
+
+  if (message.indexOf("is required") !== -1) {
+    return res
+      .status(statusCode)
+      .json({ message: `Todos os campos são obrigatórios!` });
+  }
+
   return res.status(statusCode).json({ message });
 };
